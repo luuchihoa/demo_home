6bcf174919d43bc0927490d7dfc3016a3ec32275
Luu Chi Hoa <91874475+luuchihoa@users.noreply.github.com>
2025-12-02 00:14:00 +0700

    Update writeScore.js

--- login/writeScore.js
+++ login/writeScore.js
@@ -1,8 +1,7 @@
-const API_URL =
-  'https://script.google.com/macros/s/AKfycbxi7H5MhkxM478EnIX-shg1NMxg4ljIyCcokmODv55zBnNLyTBtkKTGG-brJcSmf5Q/exec';
 
 window.writeScore = async function (userType, value) {
   const data = { user: userType, value: value };
+  const API_URL ='https://script.google.com/macros/s/AKfycbxi7H5MhkxM478EnIX-shg1NMxg4ljIyCcokmODv55zBnNLyTBtkKTGG-brJcSmf5Q/exec';
 
   const res = await fetch(API_URL, {
     method: 'POST',
