2b7fe4fdf2060646208b26bc15fe7b307ecddff0
Luu Chi Hoa <91874475+luuchihoa@users.noreply.github.com>
2025-12-01 23:39:21 +0700

    Create write.js

---
+++ login/write.js
@@ -0,0 +1,19 @@
+const API_URL =
+  'https://script.google.com/macros/s/AKfycbxi7H5MhkxM478EnIX-shg1NMxg4ljIyCcokmODv55zBnNLyTBtkKTGG-brJcSmf5Q/exec';
+
+async function sendToSheet(userType, value) {
+  const data = { user: userType, value: value };
+
+  const res = await fetch(API_URL, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(data),
+  });
+
+  const text = await res.text();
+  console.log('Server response:', text);
+}
+
+sendToSheet('admin', 'good');
