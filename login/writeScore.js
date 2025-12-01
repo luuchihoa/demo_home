d454d27d05550e2f104fb9d2a13c45122b0e2edb
Luu Chi Hoa <91874475+luuchihoa@users.noreply.github.com>
2025-12-02 00:12:56 +0700

    Update and rename write.js to writeScore.js

--- login/writeScore.js
+++ login/writeScore.js
@@ -1,7 +1,7 @@
 const API_URL =
   'https://script.google.com/macros/s/AKfycbxi7H5MhkxM478EnIX-shg1NMxg4ljIyCcokmODv55zBnNLyTBtkKTGG-brJcSmf5Q/exec';
 
-window.sendToSheet = async function (userType, value) {
+window.writeScore = async function (userType, value) {
   const data = { user: userType, value: value };
 
   const res = await fetch(API_URL, {
@@ -15,5 +15,3 @@ window.sendToSheet = async function (userType, value) {
   const text = await res.text();
   console.log('Server response:', text);
 }
-
-sendToSheet('admin', 'good');
