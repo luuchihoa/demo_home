3012ac8d8389597a8d9f30502e5772a65e3b2c0d
Luu Chi Hoa <91874475+luuchihoa@users.noreply.github.com>
2025-12-01 13:25:53 +0700

    Create script.js

---
+++ login/script.js
@@ -0,0 +1,38 @@
+const API_URL =
+  'https://script.google.com/macros/s/AKfycbzv6DuxOE5-UZQQNFOtmoknErnmJBqqHLl3fhH_x9x6sOAphju6OUnmczYPy0iElxIwXw/exec'; // Dán URL Apps Script vào đây
+
+window.login = async function () {
+  const username = document.getElementById('username').value.trim();
+  const password = document.getElementById('password').value.trim();
+  const message = document.getElementById('message');
+  if (!username || !password) {
+    message.textContent = 'Vui lòng nhập đầy đủ thông tin.';
+    return;
+  }
+
+  message.textContent = 'Đang kiểm tra...';
+
+  try {
+    const response = await fetch(API_URL, {
+      method: 'POST',
+      body: JSON.stringify({ username, password }),
+    });
+
+    const result = await response.json();
+
+    if (result.success) {
+      // lưu tên đầy đủ lại
+      localStorage.setItem('fullname', result.fullname);
+
+      message.textContent = 'Đăng nhập thành công ✔';
+
+      setTimeout(() => {
+        window.location.href = '../index.html';
+      }, 700);
+    } else {
+      message.textContent = 'Sai tài khoản hoặc mật khẩu ❌';
+    }
+  } catch (error) {
+    message.textContent = 'Lỗi kết nối!';
+  }
+};
