4fbe7958f6fa9d9abc234f94651d3431b6ce8072
Luu Chi Hoa <91874475+luuchihoa@users.noreply.github.com>
2025-12-01 13:34:53 +0700

    Update script.js

--- login/script.js
+++ login/script.js
@@ -1,3 +1,4 @@
+conso.log('TEXT');
 const API_URL =
   'https://script.google.com/macros/s/AKfycbzv6DuxOE5-UZQQNFOtmoknErnmJBqqHLl3fhH_x9x6sOAphju6OUnmczYPy0iElxIwXw/exec'; // Dán URL Apps Script vào đây
 
